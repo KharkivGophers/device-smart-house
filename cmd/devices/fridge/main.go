637359be023914f9aa81a74c45dba25d21e67b5a
vpakhuchyi <victor.pakhuchyi@gmail.com>
2017-05-10 17:28:59 +0300

    added config transfer

--- cmd/devices/fridge/main.go
+++ cmd/devices/fridge/main.go
@@ -1,20 +1,23 @@
 package main
 
 import (
+	"encoding/json"
+	"net"
 	"sync"
 
+	log "github.com/Sirupsen/logrus"
 	"github.com/vpakhuchyi/device-smart-house/devices/fridge"
 	"github.com/vpakhuchyi/device-smart-house/models"
 )
 
 var (
-	cBot    chan float32
-	cTop    chan float32
+	cBot    chan device.FridgeGenerData
+	cTop    chan device.FridgeGenerData
 	reqChan chan *models.Request
 	wg      sync.WaitGroup
 
 	//BreakerVar singletone; gives accsess to On or Off device
-	breakerVar = models.GetBreaker()
+	config = models.GetConfig()
 )
 
 //Constants fr dialup setup
@@ -25,48 +28,57 @@ const (
 )
 
 func init() {
-	cTop = make(chan float32, 2)
-	cBot = make(chan float32, 2)
+	cTop = make(chan device.FridgeGenerData, 2)
+	cBot = make(chan device.FridgeGenerData, 2)
 	reqChan = make(chan *models.Request)
 
 	//Device must be TurnedON from the beginning
-	breakerVar.SetTurned(true)
+	config.SetTurned(true)
+	config.SetCollectFreq(1)
+	config.SetSendFreq(5)
+
 }
 
 func main() {
 	//Listens for request from centre (it may contain config file)
-	// ln, _ := net.Listen(TYPE, HOST+":"+PORT)
-
-	// for {
-	// 	conn, err := ln.Accept()
-	// 	if err != nil {
-	// 		log.Errorln(err)
-	// 	}
-	// }
-
-	// swap our device state every few secs - for debugging
-	// t := time.NewTicker(time.Second * 5)
-	// go func() {
-	// 	for range t.C {
-	// 		swap(BreakerVar)
-	// 		log.Warnln("Status has changed")
-	// 	}
-	// }()
-
+	ln, _ := net.Listen(TYPE, HOST+":"+PORT)
 	wg.Add(1)
-	go device.DataGenerator(breakerVar, cBot, cTop)
-	wg.Add(1)
-	go device.DataCollector(breakerVar, cBot, cTop, reqChan)
-	wg.Add(1)
-	go device.DataTransfer(breakerVar, reqChan)
+	go func() {
+		for {
+			conn, err := ln.Accept()
+			if err != nil {
+				log.Errorln(err)
+			}
+			go handleRequest(conn, config)
+		}
+	}()
+
+	go device.DataGenerator(config, cBot, cTop)
+	go device.DataCollector(config, cBot, cTop, reqChan)
+	go device.DataTransfer(config, reqChan)
 	wg.Wait()
 }
 
-// swap func change state of device (TurnON or TurnOFF) - for debugging
-// func swap(br *models.Breaker) {
-// 	if br.GetTurned() == true {
-// 		br.SetTurned(false)
-// 	} else {
-// 		br.SetTurned(true)
-// 	}
-// }
+func handleRequest(conn net.Conn, config *models.DevConfig) {
+	var resp models.Response
+	var req models.ConfigRequest
+	err := json.NewDecoder(conn).Decode(&req)
+	if err != nil {
+		log.Errorln(err)
+	}
+
+	config.SetTurned(req.Turned)
+	config.SetCollectFreq(req.CollectFreq)
+	config.SetSendFreq(req.SendFreq)
+
+	resp.Descr = "New config accepted"
+	resp.Status = 200
+
+	err = json.NewEncoder(conn).Encode(&resp)
+	if err != nil {
+		log.Errorln(err, " Encode")
+	}
+
+	log.Println("response: ", resp)
+	log.Println("handleRequest: [done]")
+}
