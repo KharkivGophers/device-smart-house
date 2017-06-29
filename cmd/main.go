d04b66a9b6a03c322edc4bdd80a5e1e0c2d49026
Kostiantyn Yevchuk <kostyantyn.yewchuk@gmail.com>
2017-06-29 14:08:49 +0300

    Move waitgroup to main

--- cmd/main.go
+++ cmd/main.go
@@ -5,9 +5,12 @@ import (
 	"github.com/KharkivGophers/device-smart-house/models"
 	"github.com/KharkivGophers/device-smart-house/TCP"
 	"github.com/KharkivGophers/device-smart-house/devices/fridge"
+	"sync"
 )
 
 func main() {
+	var Wg sync.WaitGroup
+
 	collectData := models.CollectFridgeData{
 		CTop: make(chan models.FridgeGenerData, 100), // First Cam
 		CBot: make(chan models.FridgeGenerData, 100), // Second Cam
@@ -23,9 +26,9 @@ func main() {
 	conf := config.NewConfig()
 
 	conf.Init(configConnParams.ConnTypeConf, configConnParams.HostConf, configConnParams.PortConf)
-	collectData.Wg.Add(1)
-	go fridge.RunDataGenerator(conf, collectData.CBot, collectData.CTop, &collectData.Wg)
-	go fridge.RunDataCollector(conf, collectData.CBot, collectData.CTop, collectData.ReqChan, &collectData.Wg)
+	Wg.Add(1)
+	go fridge.RunDataGenerator(conf, collectData.CBot, collectData.CTop, &Wg)
+	go fridge.RunDataCollector(conf, collectData.CBot, collectData.CTop, collectData.ReqChan, &Wg)
 	go fridge.DataTransfer(conf, collectData.ReqChan)
-	collectData.Wg.Wait()
+	Wg.Wait()
 }
--- models/fridge.go
+++ models/fridge.go
@@ -1,7 +1,5 @@
 package models
 
-import "sync"
-
 type FridgeData struct {
 	TempCam1 map[int64]float32 `json:"tempCam1"`
 	TempCam2 map[int64]float32 `json:"tempCam2"`
@@ -16,5 +14,4 @@ type CollectFridgeData struct {
 	CBot chan FridgeGenerData
 	CTop chan FridgeGenerData
 	ReqChan chan Request
-	Wg      sync.WaitGroup // TODO to main
 }
